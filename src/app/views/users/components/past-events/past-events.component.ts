328f69c11cd0f082e8625b69e107b637540118fb
J_Theinmoliie <117365099+Theinmoliie@users.noreply.github.com>
2024-12-06 04:03:26 +0800

    portfolio done
    portfolio done

---
+++ src/app/views/users/components/past-events/past-events.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
+
+@Component({
+  selector: 'app-past-events',
+  standalone: true,
+  imports: [],
+  templateUrl: './past-events.component.html',
+  styleUrls: ['./past-events.component.css']
+})
+export class PastEventsComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private viewportScroller: ViewportScroller
+  ) {}
+
+  ngOnInit(): void {
+    // Listen for fragment changes and scroll to the fragment
+    this.route.fragment.subscribe((fragment: string) => {
+      if (fragment) {
+        this.viewportScroller.scrollToAnchor(fragment);
+      }
+    });
+  }
+}
