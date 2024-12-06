63346f4eb975dfbcf4095d567c37f9f5db0f090d
J_Theinmoliie <117365099+Theinmoliie@users.noreply.github.com>
2024-12-06 14:29:32 +0800

    nav for admin side portfolio settled
    nav component removed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: 'admin', loadChildren: () => import('./views/admin/admin.module').then(m => m.AdminModule) },
-  { path: '', redirectTo: '/admin/login', pathMatch: 'full' }, // Default route
+  // { path: '', redirectTo: '/admin/login', pathMatch: 'full' }, // Default route
   { path: 'users', loadChildren: () => import('./views/users/users.module').then(m => m.UsersModule) },
   { path: '', redirectTo: '/users/home', pathMatch: 'full' }, // Default route
 ];
