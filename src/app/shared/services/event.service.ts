40ae438b6c2ffc216225c4fc69f49a1a103ed46e
Toshi2631 <santoshdon729@gmail.com>
2025-01-21 12:35:52 +0800

    done

--- src/app/shared/services/event.service.ts
+++ src/app/shared/services/event.service.ts
@@ -4,7 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class EventService {
-  private events: any[] = [];
+  private events: any[] = JSON.parse(localStorage.getItem('events') || '[]');
+  private nextId = this.events.length > 0 ? Math.max(...this.events.map(e => e.id)) + 1 : 1;
 
   constructor() { }
 
@@ -22,5 +23,9 @@ export class EventService {
 
   deleteEvent(index: number) {
     this.events.splice(index, 1);
+  }
+
+  private saveToLocalStorage() {
+    localStorage.setItem('events', JSON.stringify(this.events));
   }
 }
